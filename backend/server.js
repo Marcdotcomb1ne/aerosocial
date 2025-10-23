d8e8376c1f9f5c92363ad612d8270516b0ebfe2c
Marcos Henry <henrymarcos08@gmail.com>
2025-10-23 06:44:38 -0300

    aaaa

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import pageRoutes from './routes/pages.routes.js';
+
+const app = express();
+const PORT = 3080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+
+// Configuração das Rotas
+app.use(pageRoutes);
+
+app.listen(PORT, () => {
+    console.log(`✅  Server is running in http://localhost:${PORT}`);
+});
+
+export default app;
