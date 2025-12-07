79839a2cb1081d320901e1a1546f54354e9e77ff
Marcos Henry <henrymarcos08@gmail.com>
2025-12-06 21:58:39 -0300

    aaa

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 // Configuração das Rotas
