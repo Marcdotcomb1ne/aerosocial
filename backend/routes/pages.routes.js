124d8786472941b848a6d0dcb0bbf6296da9c78d
Marcos Henry <henrymarcos08@gmail.com>
2025-12-20 14:59:06 -0300

    senha

--- backend/routes/pages.routes.js
+++ backend/routes/pages.routes.js
@@ -8,10 +8,19 @@ const __dirname = path.dirname(__filename);
 const pageRouter = express.Router();
 
 pageRouter.get('/', (req, res) => {
-    console.log(`\n⬆️  Rota / acessada.`);
+    console.log(`\n^-^ Rota / acessada.`);
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
+pageRouter.post('/verify-password', (req, res) => {
+    const { password } = req.body;
+    
+    if (password === process.env.SENHA) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
 
 export default pageRouter;
 
